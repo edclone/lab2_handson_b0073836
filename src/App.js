c572d75ed8bca32522ad58963f344ebe8058f937
verycoolguy999 <118308864+verycoolguy999@users.noreply.github.com>
2024-05-26 16:03:57 -0300

    got counter working but not correctly

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React  from 'react';
+import {BrowserRouter, Route, Routes,NavLink,} from 'react-router-dom';
+//pages
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+          <header>
+            <nav>
+                <h1> Welcome to my Website</h1>
+                <NavLink to="/">Home </NavLink>
+                <NavLink to="/About">About </NavLink>
+                <NavLink to="/Contact">Contact </NavLink>
+            </nav>
+          </header>
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/About" element={<About />} />
+              <Route path="/Contact" element={<Contact />} />
+            </Routes>
+          </main>
+      </BrowserRouter>
   );
 }
 
