16eb94be061ea43ac555fa1c8fc274aeb2770605
verycoolguy999 <118308864+verycoolguy999@users.noreply.github.com>
2024-05-26 19:26:41 -0300

    compilied succesfully locally

---
+++ build/precache-manifest.6eedf2435b01dbe76b2a7af4849b0bd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78a6fe2b2b4ad5d596f01b4643d0e1de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfdcacb8a661e56e46e4",
+    "url": "/static/js/2.289d3295.chunk.js"
+  },
+  {
+    "revision": "97b5938ef990e58844f0cff8514cc372",
+    "url": "/static/js/2.289d3295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09a97dc3ffc94c6ee64d",
+    "url": "/static/js/main.84099ac0.chunk.js"
+  },
+  {
+    "revision": "e19a831900bc307c357a",
+    "url": "/static/js/runtime-main.431ed013.js"
+  }
+]);
